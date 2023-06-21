d18938469d50df952887d77aed966a4a5b3f3f6d
Rajkumar <114281193+rajkumarraghunathan@users.noreply.github.com>
2023-06-21 14:33:13 +0530

    fourth commit

--- Route/auth.js
+++ Route/auth.js
@@ -3,10 +3,11 @@ const jwt = require('jsonwebtoken');
 exports.isAuth = async (req, res, next) => {
 
     const { cookies } = req;
-    // console.log(cookies);
+    console.log(cookies);
 
     if (cookies.accessToken) {
         let user = jwt.verify(cookies.accessToken, process.env.SCERET_KEY)
+        console.log(user);
         req.user = user;
         if (!req.user) {
             return res.status(401).send({ messsage: 'Not Authorized!' })
