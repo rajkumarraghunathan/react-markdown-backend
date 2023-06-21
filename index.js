dfda49ff80d62ba7a2c8a00d27bc07c44d2e12af
Rajkumar <114281193+rajkumarraghunathan@users.noreply.github.com>
2023-06-21 15:57:46 +0530

    commit

--- index.js
+++ index.js
@@ -18,8 +18,8 @@ app.use(cookieParser())
 
 
 app.use((req, res, next) => {
-    // res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-    // res.setHeader('Access-Control-Allow-Credentials', 'true');
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
 
     next();
 });
