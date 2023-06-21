8e8b32d91e3b3644d53e32ee193ec5e56128ca1e
Rajkumar <114281193+rajkumarraghunathan@users.noreply.github.com>
2023-06-21 14:40:00 +0530

    auth

--- Route/auth.js
+++ Route/auth.js
@@ -1,11 +1,12 @@
 const jwt = require('jsonwebtoken');
 
 exports.isAuth = async (req, res, next) => {
-
+    console.log(req);
     const { cookies } = req;
     console.log(cookies);
 
     if (cookies.accessToken) {
+        console.log(cookies.accessToken);
         let user = jwt.verify(cookies.accessToken, process.env.SCERET_KEY)
         console.log(user);
         req.user = user;
