4f9f053d657446f1ad9a144f3da6d1ac9c5a10b0
Rajkumar <114281193+rajkumarraghunathan@users.noreply.github.com>
2023-06-25 10:35:07 +0530

    Final commit

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const app = express()
 app.use(express.json());
 app.use(cookieParser())
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: 'https://lighthearted-toffee-fb9353.netlify.app',
     credentials: true,
 }))
 
