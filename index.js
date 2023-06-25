e3c84188a9b8f55f23acaadbcc9f43d3fab22761
Rajkumar <114281193+rajkumarraghunathan@users.noreply.github.com>
2023-06-25 10:38:53 +0530

    Final commit

--- index.js
+++ index.js
@@ -19,8 +19,8 @@ app.use(cors({
 
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-    res.setHeader('Access-Control-Allow-Credentials', 'true');
+    res.header('Access-Control-Allow-Origin', 'https://lighthearted-toffee-fb9353.netlify.app');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
 
     next();
 });
